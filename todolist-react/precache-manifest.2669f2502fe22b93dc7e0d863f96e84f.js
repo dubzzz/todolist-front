4ffd3008e41ae97fb561ea95db881175bc630630
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-01 01:14:36 +0000

    Deploy dubzzz/todolist-front to github.com/dubzzz/todolist-front.git:gh-pages

---
+++ todolist-react/precache-manifest.2669f2502fe22b93dc7e0d863f96e84f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab718fbaca94c8336e79114ec9136b7f",
+    "url": "/todolist-front/todolist-react/index.html"
+  },
+  {
+    "revision": "7008785fcca7d1169397",
+    "url": "/todolist-front/todolist-react/static/js/2.5ab34a31.chunk.js"
+  },
+  {
+    "revision": "23d5fd035252b2e9c491",
+    "url": "/todolist-front/todolist-react/static/js/3.d438fa5b.chunk.js"
+  },
+  {
+    "revision": "af913b444f96a5ab9541",
+    "url": "/todolist-front/todolist-react/static/js/4.80ca0620.chunk.js"
+  },
+  {
+    "revision": "05aa4ae6fae86ab0db7f",
+    "url": "/todolist-front/todolist-react/static/js/5.3a2f51d4.chunk.js"
+  },
+  {
+    "revision": "9cc92ba9cbb04cc0bec4",
+    "url": "/todolist-front/todolist-react/static/js/main.e44aa170.chunk.js"
+  },
+  {
+    "revision": "70999bbf11d77e1afcea",
+    "url": "/todolist-front/todolist-react/static/js/runtime~main.40965b3f.js"
+  }
+]);
