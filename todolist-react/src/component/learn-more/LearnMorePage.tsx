6b4f874ebe7591b45006ace7e6a0f541d4df18fd
Nicolas DUBIEN <github@dubien.org>
2019-08-01 20:28:45 +0200

    Fill learn more section

--- todolist-react/src/component/learn-more/LearnMorePage.tsx
+++ todolist-react/src/component/learn-more/LearnMorePage.tsx
@@ -4,5 +4,26 @@ import Typography from '@material-ui/core/Typography';
 type Props = {};
 
 export default function LearnMorePage(props: Props) {
-  return <Typography>Hello</Typography>;
+  return (
+    <>
+      <Typography variant="h4" component="h2">
+        App
+      </Typography>
+      <Typography>
+        This app makes you able to maintain your todos up-to-date. Multiple users can access and edit the todos in
+        parallel*.
+      </Typography>
+      <Typography variant="subtitle2">
+        Parallel: users have to share the same local storage (be on same browser)
+      </Typography>
+      <Typography variant="h4" component="h2">
+        APIs
+      </Typography>
+      <Typography>
+        For the sake of the demo, all the API calls are mocked. In order to be as realistic as possible they are delayed
+        and subject to failures. Any data that needs to be persisted such as authentification tokens or todos are stored
+        within the local storage of the browser in use.
+      </Typography>
+    </>
+  );
 }
