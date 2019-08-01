c2b9dc525500339f4b2d228e650532fde2b545b1
Nicolas DUBIEN <github@dubien.org>
2019-08-01 20:11:28 +0200

    Preload authentificated app when on App

--- todolist-react/src/App.tsx
+++ todolist-react/src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { withAuthentificated } from './hoc/Authentificated';
 import { AuthentificationProvider } from './context/AuthentificationContext';
 import LoginPage from './component/login/LoginPage';
 
-const AppPage = React.lazy(() => import('./component/app/AppPage'));
+const loadAppPage = () => import('./component/app/AppPage');
+const AppPage = React.lazy(loadAppPage);
 
 const App: React.FC = () => {
+  useEffect(() => {
+    loadAppPage();
+  }, []);
   return (
     <div className="App">
       <AuthentificationProvider>
