9eb0ecee3eee6d775fc721b9210999e7dbaee697
Nicolas DUBIEN <github@dubien.org>
2019-08-06 19:19:38 +0200

    Update app.module.ts

--- todolist-angular/src/app/app.module.ts
+++ todolist-angular/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { APP_BASE_HREF } from "@angular/common";
-import { environment } from "./environments/environment";
+import { environment } from "../environments/environment";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
