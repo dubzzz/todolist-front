3c7e00ca37ab0c9dba2ca049110adcc07e07df24
Nicolas DUBIEN <github@dubien.org>
2019-08-06 22:55:13 +0200

    Add navigation from menu

--- todolist-angular/src/app/authenticated/authenticated-menu.component.ts
+++ todolist-angular/src/app/authenticated/authenticated-menu.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-authenticated-menu",
   templateUrl: "./authenticated-menu.component.html",
   styleUrls: ["./authenticated-menu.component.css"]
 })
-export class AuthenticatedMenuComponent {}
+export class AuthenticatedMenuComponent {
+  constructor(public router: Router) {}
+
+  goTo(pageLink: string) {
+    this.router.navigate([pageLink]);
+  }
+}
