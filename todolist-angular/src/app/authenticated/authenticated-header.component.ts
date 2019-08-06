2e8b808367ea98ad4d29e835f83bbef2537b5d90
Nicolas DUBIEN <github@dubien.org>
2019-08-06 22:59:37 +0200

    Connect header to AuthService

--- todolist-angular/src/app/authenticated/authenticated-header.component.ts
+++ todolist-angular/src/app/authenticated/authenticated-header.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { AuthService } from "../auth/auth.service";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-authenticated-header",
   templateUrl: "./authenticated-header.component.html",
   styleUrls: ["./authenticated-header.component.css"]
 })
-export class AuthenticatedHeaderComponent {
+export class AuthenticatedHeaderComponent implements OnInit {
   @Input() expandedMenu: boolean;
   @Output() toggleMenu = new EventEmitter<void>();
+
+  username$: Observable<string>;
+
+  constructor(readonly authService: AuthService) {}
+
+  ngOnInit() {
+    this.username$ = this.authService.state$.pipe(map(s => s.username));
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
