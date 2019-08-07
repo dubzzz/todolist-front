286d6d8b345b2fb37887f6ffd113c5359e9adbde
Nicolas DUBIEN <github@dubien.org>
2019-08-07 23:17:53 +0200

    Implement not found page

--- todolist-angular/src/app/authenticated/not-found/not-found/not-found.component.ts
+++ todolist-angular/src/app/authenticated/not-found/not-found/not-found.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-not-found",
   templateUrl: "./not-found.component.html",
   styleUrls: ["./not-found.component.css"]
 })
-export class NotFoundComponent implements OnInit {
-  constructor() {}
+export class NotFoundComponent {
+  constructor(readonly router: Router) {}
 
-  ngOnInit() {}
+  backHome() {
+    this.router.navigate(["/"]);
+  }
 }
