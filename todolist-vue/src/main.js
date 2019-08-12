dd053769afa4df6ec2bddc622c99aedb35e5f27d
Nicolas DUBIEN <github@dubien.org>
2019-08-12 23:02:21 +0200

    Add vue-material

--- todolist-vue/src/main.js
+++ todolist-vue/src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
 import App from "./App.vue";
 
 Vue.use(VueRouter);
