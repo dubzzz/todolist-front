1f381e7208992d935574e1057811bdeea059e3af
Nicolas DUBIEN <github@dubien.org>
2019-08-12 23:10:29 +0200

    Basic router setup

--- todolist-vue/src/main.js
+++ todolist-vue/src/main.js
@@ -5,11 +5,19 @@ import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
 import App from "./App.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
+const router = new VueRouter({
+  base: process.env.NODE_ENV === "production" ? "/todolist-vue/" : "/",
+  mode: "history",
+  routes: [{ path: "/", name: "home", component: HelloWorld }]
+});
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
