6c4d3d78485ff961e00199dd2666061e5b1349b7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-14 00:34:13 +0000

    Deploy dubzzz/todolist-front to github.com/dubzzz/todolist-front.git:gh-pages

--- todolist-vue/js/chunk-2d0d3c74.779b08f2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d3c74"],{"5de4":function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("LoginPage to "+e._s(e.redirect))])},c=[],i={name:"LoginPage",props:{redirect:String}},a=i,o=t("2877"),d=Object(o["a"])(a,r,c,!1,null,"563bdc0a",null);n["default"]=d.exports}}]);
-//# sourceMappingURL=chunk-2d0d3c74.779b08f2.js.map
