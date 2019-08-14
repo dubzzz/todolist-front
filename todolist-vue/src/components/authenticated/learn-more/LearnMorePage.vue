99320ca61536d937d124996a96fe35f70f9f3fbe
Nicolas DUBIEN <github@dubien.org>
2019-08-14 20:24:38 +0200

    Add content for learn-more

--- todolist-vue/src/components/authenticated/learn-more/LearnMorePage.vue
+++ todolist-vue/src/components/authenticated/learn-more/LearnMorePage.vue
@@ -1,10 +1,50 @@
 <template>
-  <div>LearnMorePage</div>
+  <div>
+    <h4>App</h4>
+    <p>
+      This app makes you able to maintain your todos up-to-date. Multiple users
+      can access and edit the todos in parallel*.
+    </p>
+    <p>
+      <em>
+        Parallel: users have to share the same local storage (be on same
+        browser)
+      </em>
+    </p>
+    <h4>APIs</h4>
+    <p>
+      For the sake of the demo, all the API calls are mocked. In order to be as
+      realistic as possible they are delayed and subject to failures. Any data
+      that needs to be persisted such as Authentication tokens or todos are stored
+      within the local storage of the browser in use.
+    </p>
+    <h4>Revoke token</h4>
+    <p>
+      The token used and stored to keep the user connected to the API can be
+      revoked by updating the value of
+      <em>window.validToken</em> in the console
+      or by clicking
+      <br />
+      <md-button class="md-primary" v-on:click="resetToken()">here</md-button>.
+    </p>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import { MdButton } from "vue-material/dist/components";
+
+Vue.use(MdButton);
+
 export default {
-  name: "LearnMorePage"
+  name: "LearnMorePage",
+  methods: {
+    resetToken() {
+      window.validToken = Math.random()
+        .toString(16)
+        .substr(2);
+    }
+  }
 };
 </script>
 
