c1889a175dc68a06a087a3778a98920b388633dd
Nicolas DUBIEN <github@dubien.org>
2019-08-27 00:20:06 +0200

    Use TodoState structure to read the status

--- todolist-vue/src/components/authenticated/list/TodoListListEntry.vue
+++ todolist-vue/src/components/authenticated/list/TodoListListEntry.vue
@@ -3,14 +3,14 @@
     <md-card-content class="todo-wrapper">
       <md-checkbox
         v-bind:value.sync="!content.done"
-        v-bind:disabled="syncState !== 'noop'"
+        v-bind:disabled="!canEdit"
         v-on:change="toggleTodo()"
       ></md-checkbox>
       <span
         class="todo-label"
-        v-bind:class="{ remove: (syncState === 'remove'), noaction: (syncState !== 'noop') }"
+        v-bind:class="{ remove: onGoingRemoval, noaction:!canEdit }"
       >{{content.task}}</span>
-      <md-button v-if="syncState !== 'noop'">
+      <md-button v-if="!canEdit">
         <md-icon>refresh</md-icon>
       </md-button>
       <md-button v-on:click="removeTodo()">
@@ -21,9 +21,19 @@
 </template>
 
 <script>
+import { TodoState } from "../../../store/models/todolist";
+
 export default {
   name: "TodoListListEntry",
   props: { syncState: String, content: Object },
+  computed: {
+    canEdit() {
+      return this.syncState === TodoState.Noop;
+    },
+    onGoingRemoval() {
+      return this.syncState === TodoState.Remove;
+    }
+  },
   methods: {
     toggleTodo() {
       const token = this.$store.state.authentication.token;
